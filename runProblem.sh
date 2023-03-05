48044ccb2d667010bc8874c4d1626b290ba816e0
swansoat5662 <ats5662@psu.edu>
2023-03-05 11:18:02 -0500

    fix errors

--- runProblem.sh
+++ runProblem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 7z x LidinoidLinerMeshMeterTet4.7z
-cfs -t8 -d Lidinoid_NIT_HarmonicAcousticSim_InputDeck
+cfs -t4 -d Lidinoid_NIT_HarmonicAcousticSim_InputDeck
 rm *.cdb
 cp history/* .
 rm -rf history
